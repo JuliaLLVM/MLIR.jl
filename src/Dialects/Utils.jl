efbc1a998275f766c1d371c691c7dd51c1693851
Sergio Sánchez Ramírez <mofeing@gmail.com>
2026-05-22 09:27:07 -0500

    delete "Dialects/Utils.jl" file

--- src/Dialects/Utils.jl
+++
@@ -1,4 +0,0 @@
-import ..IR: NamedAttribute
-
-operandsegmentsizes(segments) = NamedAttribute("operand_segment_sizes", Int32.(segments))
-resultsegmentsizes(segments) = NamedAttribute("result_segment_sizes", Int32.(segments))
